65d1e02a1b420184322f697417a0af734c34b3ee
unknown <jimpidas>
2021-05-12 19:20:50 +0530

    [Jyosmita]:Added the code where a single player starts the game from 0 position

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLadderSimulator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int PositionOfPlayer = 0;
+
+            Console.WriteLine("Welcome to the snake and ladder simulator using C#");
+            Console.WriteLine("Position of player at initial is " + PositionOfPlayer);
+        }
+    }
+}
+
